04bf14fe5db1b064d0e6352f65279625290ab690
Ananya Jain <ananyaj1515@gmail.com>
2026-05-26 10:49:39 +0800

    Add data fields to view model

--- client/ViewModels/MainWindowViewModel.cs
+++ client/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace client.ViewModels;
+﻿using System.Collections.ObjectModel;
+using client.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace client.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private bool isConnected;
+
+    // this doesnt need observable property, as oc alr notifies when items/added or removed, would need if we would be changing the whole collection
+    public ObservableCollection<Message> messages {get;} = new();
+
+    [ObservableProperty]
+    private string inputText = string.Empty;
+
+    [ObservableProperty]
+    private string username = string.Empty;
+    
+     private const string ServerIP = "127.0.0.0";
+     private const int Port = 9000;
 }
